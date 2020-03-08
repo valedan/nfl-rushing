1cec8e4e4d3286e29a5497638c0b3795806cce96
Dan <dan@danmolloy.dev>
2020-03-08 12:09:56 -0400

    setup frontend dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Stats } from "./components/Stats/Stats";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { ThemeProvider } from "@material-ui/styles";
 
-function App() {
+const apollo = new ApolloClient();
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={apollo}>
+      <Router>
+        <ThemeProvider>
+          <Switch>
+            <Route path="/">
+              <Stats />
+            </Route>
+          </Switch>
+        </ThemeProvider>
+      </Router>
+    </ApolloProvider>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/components/Stats/Stats.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import Paper from "@material-ui/core/Paper";
+
+export const Stats = () => {
+  return (
+    <Wrapper>
+      <p>Stats</p>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  color: blue;
+`;
